fd85c35708eb77d9b12201fbbfea417a30fb8a82
tloidev1208 <thanh.loi@bs.io.vn>
2025-12-29 08:55:51 +0700

    remove BE

--- BE/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dt5uqjhrm", 
-  api_key: "361364894778695",      
-  api_secret: "ISLZO_pCIt0p88QhtnnbYS6EcVg",
-});
-
-module.exports = cloudinary;
